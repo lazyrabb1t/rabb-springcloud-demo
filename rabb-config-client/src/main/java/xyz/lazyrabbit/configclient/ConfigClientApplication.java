1129b4eafe885899a9a459483fa00196b0802692
lazyrabbit <liangxinyjd@163.com>
2021-02-15 13:56:48 +0800

    添加config模块

---
+++ rabb-config-client/src/main/java/xyz/lazyrabbit/configclient/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package xyz.lazyrabbit.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    @Value("${spring.application.name}")
+    String name;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @GetMapping
+    public String url() {
+        return name;
+    }
+}
---
+++ rabb-config-client/src/test/java/xyz/lazyrabbit/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lazyrabbit.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
